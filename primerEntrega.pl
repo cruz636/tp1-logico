bdca4c7768cbd708c7babd5faa2785b326727df5
cruz636 <jcruz96@outlook.es>
2017-06-10 15:29:01 -0300

    Create primerEntrega.pl

--- primerEntrega.pl
+++ primerEntrega.pl
@@ -8,33 +8,53 @@ programa(santiago,ecmsScript).
 rol(fernando,analistaFuncional).
 rol(andres,projectLeader).
 
-persona(Alguien):- esProgramador(Alguien).
-persona(Alguien):- rol(Alguien,_).
-
 lenguaje(cobol).
 lenguaje(visualBasic).
 lenguaje(java).
 lenguaje(puntoNet).
 lenguaje(ecmsScript).
 
+proyecto(sumatra,[julieta,marcos,andres],[java,puntoNet]).
+proyecto(prometeus,[fernando,santiago],[cobol]).
+
+
+persona(Alguien):- esProgramador(Alguien).
+persona(Alguien):- rol(Alguien,_).
+
 esProgramador(Alguien):- programa(Alguien,_).
 
+lenguajeProyecto(Proyecto,Lista):-
+	esProyecto(Proyecto),
+	proyecto(Proyecto,_,Lista).
 
-proyecto(sumatra,[julieta,marcos,andres],[cobol]).
-proyecto(prometeus,[fernando,santiago],[java,puntoNet]).
 esProyecto(Proyecto):- proyecto(Proyecto,_,_).
 
+perteneceA(sumatra,Persona):-
+	findall(Persona,proyecto(sumatra,Persona,_),ListaDe),
+	flatten(ListaDe,Lista),
+	member(Persona,Lista).
+
+perteneceA(prometeus,Persona):-
+	findall(Persona,proyecto(prometeus,Persona,_),ListaDe),
+	flatten(ListaDe,Lista),
+	member(Persona,Lista).
+
+
 estaOk(Persona,Proyecto):- esProyecto(Proyecto),
+	perteneceA(Proyecto,Persona),
 	rol(Persona,analistaFuncional).
 
 estaOk(Persona,Proyecto):- esProyecto(Proyecto),
+	perteneceA(Proyecto,Persona),
 	rol(Persona,projectLeader).
 
 estaOk(Persona,Proyecto):-
-	persona(Persona),
-  esProyecto(Proyecto),
+	perteneceA(Proyecto,Persona),
+	esProyecto(Proyecto),
 	programa(Persona,Lenguaje),
 	proyecto(Proyecto,_,Lista),
 	member(Lenguaje,Lista).
 
 
+miembrosBienAsignados(Lista):-
+	findall(Persona,estaOk(Persona,_),Lista).
