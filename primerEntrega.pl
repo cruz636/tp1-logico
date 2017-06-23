5e99908270efb8dd284f99ce799220494155cc18
cruz636 <jcruz96@outlook.es>
2017-06-23 14:22:19 -0300

    Update primerEntrega.pl

--- primerEntrega.pl
+++ primerEntrega.pl
@@ -4,69 +4,41 @@ programa(fernando,java).
 programa(julieta,java).
 programa(marcos,java).
 programa(santiago,java).
-programa(santiago,ecmsScript).
+programa(santiago,emcascript).
+
+
 rol(fernando,analistaFuncional).
 rol(andres,projectLeader).
-
-lenguaje(cobol).
-lenguaje(visualBasic).
-lenguaje(java).
-lenguaje(puntoNet).
-lenguaje(ecmsScript).
+rol(Alguien,programador):- programa(Alguien,_).
 
 proyecto(sumatra,[julieta,marcos,andres],[java,puntoNet]).
 proyecto(prometeus,[fernando,santiago],[cobol]).
-
-persona(Alguien):- esProgramador(Alguien).
-persona(Alguien):- rol(Alguien,_).
-
-esProgramador(Alguien):- programa(Alguien,_).
-
-lenguajeProyecto(Proyecto,Lista):-
-	esProyecto(Proyecto),
-	proyecto(Proyecto,_,Lista).
-
-esProyecto(Proyecto):- proyecto(Proyecto,_,_).
-
+	
 perteneceA(sumatra,Persona):-
-	findall(Personas,proyecto(sumatra,Personas,_),ListaDe),
-	flatten(ListaDe,Lista),
-	member(Persona,Lista).
+	proyecto(sumatra,ListaDePersonas,_),
+	member(Persona,ListaDePersonas).
 
 perteneceA(prometeus,Persona):-
-	findall(Personas,proyecto(prometeus,Personas,_),ListaDe),
-	flatten(ListaDe,Lista),
-	member(Persona,Lista).
+	proyecto(prometeus,ListaDePersonas,_),
+	member(Persona,ListaDePersonas).
 
 programadorDe(Proyecto,Persona):-
-	findall(Personas,proyecto(Proyecto,Personas,_),ListaDe),
-	flatten(ListaDe,Lista),
+	proyecto(Proyecto,Lista,_),
 	member(Persona,Lista),
-	esProgramador(Persona).
+	rol(Persona,programador).
 
-estaOk(Persona,Proyecto):- esProyecto(Proyecto),
+estaBienAsignado(Persona,Proyecto):-
 	perteneceA(Proyecto,Persona),
 	rol(Persona,analistaFuncional).
 
-estaOk(Persona,Proyecto):- esProyecto(Proyecto),
+estaBienAsignado(Persona,Proyecto):-
 	perteneceA(Proyecto,Persona),
 	rol(Persona,projectLeader).
 
-estaOk(Persona,Proyecto):-
+estaBienAsignado(Persona,Proyecto):-
 	perteneceA(Proyecto,Persona),
-	esProyecto(Proyecto),
 	programa(Persona,Lenguaje),
 	proyecto(Proyecto,_,Lista),
 	member(Lenguaje,Lista).
-
-miembrosBienAsignadosA(sumatra,Lista):-
-	findall(Persona,estaOk(Persona,sumatra),Lista).
-
-miembrosBienAsignadosA(prometeus,Lista):-
-	findall(Persona,estaOk(Persona,prometeus),Lista).
-
-miembrosBienAsignados(Lista):-
-	esProyecto(Proyecto),
-	findall(Personas,miembrosBienAsignadosA(Proyecto,Personas),Miembros),
-	flatten(Miembros,Lista).
+	
 	
